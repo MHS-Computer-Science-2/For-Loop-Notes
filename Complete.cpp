5ec9f96456652b936c8a4e202d69f56f0d531152
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-28 15:51:34 -0500

    Add files via upload

---
+++ Complete.cpp
@@ -0,0 +1,37 @@
+// FOR LOOP Notes
+//   I CAN use the C++ INCREMENT and DECREMENT operators
+//   I CAN create C++ FOR loopsand use them to solve problems
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Use a FOR LOOP to display 1 - 10
+	for (int i = 1; i <= 10; i++) {
+		cout << i << endl;
+	}
+
+	/*FOR LOOP STRUCTURE:
+		int i = 1   -> makes a new variable for the loop
+		i <= 10     -> loops WHILE i <= 10
+		i++         -> Increments i at the end of the loop
+	*/
+
+	//YOU TRY:
+	//  Display the numbers 99 to 0 counting down by 1s
+	//  using a FOR LOOP
+	for (int i = 99; i >= 0; i--) {
+		cout << i << endl;
+	}
+
+
+	//YOU TRY:
+	//  Display the multiple of 5 from 500 to 1000
+	//  using a FOR LOOP
+	for (int i = 500; i <= 1000; i = i +5) {
+		cout << i << endl;
+	}
+
+}
+
---
+++ ForLoops.cpp
@@ -0,0 +1,33 @@
+// FOR LOOP Notes
+//   I CAN use the C++ INCREMENT and DECREMENT operators
+//   I CAN create C++ FOR loopsand use them to solve problems
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Use a FOR LOOP to display 1 - 10
+	for (int i = 1; i <= 10; i++) {
+		cout << i << endl;
+	}
+
+	/*FOR LOOP STRUCTURE:
+		int i = 1   -> makes a new variable for the loop
+		i <= 10     -> loops WHILE i <= 10
+		i++         -> Increments i at the end of the loop
+	*/
+
+	//YOU TRY:
+	//  Display the numbers 99 to 0 counting down by 1s
+	//  using a FOR LOOP
+
+
+
+	//YOU TRY:
+	//  Display the multiple of 5 from 500 to 1000
+	//  using a FOR LOOP
+
+
+}
+
